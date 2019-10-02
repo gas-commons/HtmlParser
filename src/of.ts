455661a562e75f563262b52490d2051b389606ee
Munieru <20086673+munierujp@users.noreply.github.com>
2019-10-02 14:35:49 +0900

    Replace standard to standard-with-typescript (#3)
    * Update packages
    
    * Add eslint-config-standard-with-typescript package
    
    `npm i -D eslint-config-standard-with-typescript`
    
    * Fix lint/fix command
    
    * Add tsconfig.eslint.json
    
    * Use standard-with-typescript
    
    * Remove eslint-config-standard package
    
    `npm rm eslint-config-standard`
    
    * Apply lint

--- src/of.ts
+++ src/of.ts
@@ -1,8 +1,8 @@
 import HtmlParser_ from './HtmlParser_'
 
 // NOTE: Below function will be public in Google Apps Script.
-// eslint-disable-next-line camelcase
-function of (value: GoogleAppsScript.XML_Service.Element) {
+// eslint-disable-next-line @typescript-eslint/camelcase
+function of (value: GoogleAppsScript.XML_Service.Element): HtmlParser_ {
   return new HtmlParser_(value)
 }
 
